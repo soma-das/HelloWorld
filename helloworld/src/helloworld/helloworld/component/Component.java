19b00a4974e1a24c9d3279f1521980abdfa894e3
soma <soma@DESKTOP-OSBUM91>
2018-01-31 18:01:15 +0100

    hello world reactive Block

---
+++ helloworld/src/helloworld/helloworld/component/Component.java
@@ -0,0 +1,11 @@
+package helloworld.helloworld.component;
+
+import no.ntnu.item.arctis.runtime.Block;
+
+public class Component extends Block {
+
+	public void helloWorld() {
+		System.out.println("Hello World");
+	}
+
+}
